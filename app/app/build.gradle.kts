74729100ee62cabde628d382f9a1a61909de4cef
Luminexa <Luminexa@Gmail.com>
2026-06-29 14:41:06 +0800

    feat(app): support injecting versionCode and versionName via project properties

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.example.sheeps"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = (project.findProperty("versionCode") as? String)?.toIntOrNull() ?: 1
+        versionName = (project.findProperty("versionName") as? String) ?: "1.0"
     }
 
     buildTypes {
