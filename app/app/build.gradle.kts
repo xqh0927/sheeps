cdc5b6fcc1a7fcea3a8f5b6bc9645948dd250ddb
Luminexa <Luminexa@Gmail.com>
2026-06-29 15:41:31 +0800

    feat(release): filter workflows by commit msg and restore release.js auto submit

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.example.sheeps"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 1
+        versionName = "1.0.1"
     }
 
     buildTypes {
