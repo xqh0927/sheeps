cde8e9db74bf3b2b8d11c4d863ea56f2911323df
Luminexa <Luminexa@Gmail.com>
2026-06-30 15:26:16 +0800

    feat: 功能迭代

---
+++ app/core/src/main/java/com/example/sheeps/core/utils/LanguageUtils.kt
@@ -0,0 +1,53 @@
+package com.example.sheeps.core.utils
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.sheeps.core.R
+
+@Composable
+fun getLocalizedItemName(itemType: String): String {
+    return when (itemType.uppercase()) {
+        "UNDO" -> stringResource(id = R.string.item_undo)
+        "MOVEOUT" -> stringResource(id = R.string.item_moveout)
+        "SHUFFLE" -> stringResource(id = R.string.item_shuffle)
+        "REVIVE" -> stringResource(id = R.string.item_revive)
+        "HINT" -> stringResource(id = R.string.item_hint)
+        "BOMB" -> stringResource(id = R.string.item_bomb)
+        "JOKER" -> stringResource(id = R.string.item_joker)
+        "DOUBLE_POINTS" -> stringResource(id = R.string.item_double_points)
+        "SKIN_INK" -> stringResource(id = R.string.item_skin_ink)
+        "SKIN_CYBER" -> stringResource(id = R.string.item_skin_cyber)
+        else -> itemType
+    }
+}
+
+@Composable
+fun getLocalizedSource(source: String): String {
+    if (source.startsWith("UNLOCK_LEVEL_")) {
+        val levelId = source.substringAfter("UNLOCK_LEVEL_")
+        return stringResource(id = R.string.points_source_unlock_level, levelId)
+    }
+    if (source.startsWith("SHOP_REDEEM_")) {
+        val itemType = source.substringAfter("SHOP_REDEEM_")
+        return stringResource(id = R.string.points_source_shop_redeem, getLocalizedItemName(itemType))
+    }
+    if (source.startsWith("DAILY_TASK_")) {
+        val taskNameKey = source.substringAfter("DAILY_TASK_")
+        return stringResource(id = R.string.points_source_daily_task, getLocalizedTaskName(taskNameKey))
+    }
+    return when (source.uppercase()) {
+        "FIRST_CLEAR" -> stringResource(id = R.string.points_source_first_clear)
+        "SIGN_IN" -> stringResource(id = R.string.points_source_sign_in)
+        else -> source
+    }
+}
+
+@Composable
+fun getLocalizedTaskName(taskName: String): String {
+    return when (taskName.uppercase()) {
+        "PLAY_3_GAMES", "PLAY_3" -> stringResource(id = R.string.task_play_3)
+        "PLAY_5_GAMES", "PLAY_5" -> stringResource(id = R.string.task_play_5)
+        "SIGN_IN" -> stringResource(id = R.string.task_sign_in)
+        else -> taskName
+    }
+}
