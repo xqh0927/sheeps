d2bc55f5dc6bad6a8ec92d0191992e91d08de993
Luminexa <Luminexa@Gmail.com>
2026-07-03 10:57:48 +0800

    feat:降低版本 测试自动化打包

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.example.sheeps"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.6"
+        versionCode = 5
+        versionName = "1.0.5"
     }
 
     buildTypes {
