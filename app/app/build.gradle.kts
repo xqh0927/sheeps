ff48c916d0f3a5110d70df04d2cad15a5b398471
Luminexa <Luminexa@Gmail.com>
2026-07-03 19:48:21 +0800

    chore(release): bump version to v1.0.7 & update release notes

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.example.sheeps"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.6"
+        versionCode = 7
+        versionName = "1.0.7"
     }
 
     buildTypes {
